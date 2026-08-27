cf2927dd0c11c9af3d326d33d688dca4b8596ba8
Richmond Asante <asanterich555@gmail.com>
2026-08-27 08:12:02 -0400

    Update TaskFlow branding and prepare demo

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -6,6 +6,6 @@ console.log(import.meta.env.VITE_API_BASE_URL)
  const API=axios.create({
    
     baseURL: import.meta.env.VITE_API_BASE_URL, 
-     timeout: 10000, //10 seconds timeout for requests
+     timeout: 30000, //10 seconds timeout for requests
 })
 export default API
